55926bdeb78cb6aa07170420c7b5fba7740c73fb
Kanatello7 <jetrukanatello@gmail.com>
2025-12-05 17:01:14 +0500

    add agg command to fetch rss

--- command_handlers.go
+++ command_handlers.go
@@ -80,6 +80,15 @@ func handlerListUsers(s *state, cmd command) error {
 	return nil
 }
 
+func handlerAgg(s *state, cmd command) error {
+	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if err != nil {
+		return fmt.Errorf("couldn't fetch feed: %w", err)
+	}
+	fmt.Printf("Feed: %+v\n", feed)
+	return nil
+}
+
 func printUser(user database.User) {
 	fmt.Printf(" * ID:      %v\n", user.ID)
 	fmt.Printf(" * Name:    %v\n", user.Name)
